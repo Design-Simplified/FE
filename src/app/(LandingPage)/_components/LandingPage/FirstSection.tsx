1195164c3e4388675d7805d0a8e6f0b1da480be9
qiqioberon <aquq1q1.farrukh@gmail.com>
2025-01-22 21:15:00 +0700

    feat: add section 1 desktop landing page

--- src/app/(LandingPage)/_components/LandingPage/FirstSection.tsx
+++ src/app/(LandingPage)/_components/LandingPage/FirstSection.tsx
@@ -4,8 +4,8 @@ import Typography from "@/components/Typography";
 
 export default function FirstSection() {
   return (
-    <div className="bg-white w-full py-10 px-5 flex flex-col gap-5 items-center justify-center">
-      <div className="flex w-full flex-col gap-4">
+    <div className="bg-white w-full py-10 pb-20 px-5 flex flex-col sm:px-16 lg:px-28 sm:py-36 sm:flex-row gap-5 sm:gap-0 items-center justify-center">
+      <div className="flex w-full flex-col gap-4 sm:gap-8	">
         <div className="Proven flex flex-row gap-2 items-center justify-start w-full">
           <NextImage
             src="/LandingPage/4UserFoto.png"
@@ -17,7 +17,7 @@ export default function FirstSection() {
             font="Lora"
             variant="h5"
             weight="semibold"
-            className="text-[#525B44] text-sm"
+            className="text-[#525B44] text-sm sm:text-xs md:text-sm lg:text-xl xl:text-2xl"
           >
             +1500 Users Have Already Proven it
           </Typography>
@@ -26,7 +26,7 @@ export default function FirstSection() {
           font="Lora"
           variant="h1"
           weight="bold"
-          className="text-[26px]/8"
+          className="text-[26px]/8 sm:text-xl md:text-2xl lg:text-[32px]/[50px] xl:text-[45px]/[57px] text-black"
         >
           The Definitive Game Changer in Buying and Selling Templates.
         </Typography>
@@ -34,41 +34,44 @@ export default function FirstSection() {
           font="Lora"
           variant="p"
           weight="medium"
-          className="text-slate-500"
+          className="text-slate-500 text-xs lg:text-lg xl:text-2xl sm:text-xs md:text-xs"
         >
           From discovering the perfect design to showcasing your creativity, we
           redefine the game in buying and selling templates.
         </Typography>
         <div className="button flex flex-row gap-2">
-          <Button variant="green">
+          <Button variant="green" className="px-2 py-0.5 sm:px-4 sm:py-1">
             <Typography
               font="Lora"
               variant="p"
               weight="semibold"
-              className="text-white"
+              className="text-white text-[9px] md:text-sm lg:text-xl"
             >
               Explore Now
             </Typography>
           </Button>
-          <Button variant="outline" className="border border-black">
+          <Button
+            variant="outline"
+            className="border px-2 py-0.5 border-black sm:px-4 sm:py-1"
+          >
             <Typography
               font="Lora"
               variant="p"
               weight="semibold"
-              className="text-black"
+              className="text-black text-[9px] md:text-sm lg:text-xl"
             >
               Start Selling
             </Typography>
           </Button>
         </div>
       </div>
-      <div className="image relative w-full">
+      <div className="image relative w-full min-[500px]:w-[90%] sm:w-full lg:w-[80%] xl:w-[70%]">
         <NextImage
           src="/LandingPage/Section1Mobile.png"
           width={221}
           height={190}
           alt="Section1Foto"
-          className="absolute -right-5 w-[56%]"
+          className="absolute -right-5 w-full min-[500px]:-right-11 sm:-right-16 lg:-right-28 sm:-top-40 md:-top-40 lg:-top-48 xl:-top-56"
           imgClassName="object-cover w-full"
         />
       </div>
