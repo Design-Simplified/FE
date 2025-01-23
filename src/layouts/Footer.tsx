32f112d02d8c2ba0443aaf9bd31db8c2effc7272
qiqioberon <aquq1q1.farrukh@gmail.com>
2025-01-23 12:08:39 +0700

    feat: add footer desktop

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -67,31 +67,94 @@ const FooterMenu = [
   },
 ];
 
-export default function Footer() {
+const FooterMobileMenu = () => {
   return (
-    <footer className="w-full relative flex flex-col gap-16 px-8 py-10 bg-[#525B44]">
-      <div className="w-full flex flex-col gap-10">
-        <div className="each flex w-full gap-8 flex-col">
-          {FooterMenu.map((menu: FooterMenuProps, index) => (
-            <div key={index} className="each flex w-full gap-8 flex-col">
+    <>
+      <div className="each flex w-full gap-8 flex-col sm:hidden">
+        {FooterMenu.map((menu: FooterMenuProps, index) => (
+          <div key={index} className="each flex w-full gap-8 flex-col">
+            <Typography
+              font="Lora"
+              weight="bold"
+              className="text-xl text-white"
+            >
+              {menu.title}
+            </Typography>
+            <div
+              className={clsxm(
+                "isi w-full flex flex-col gap-3",
+                menu.title === "Payment" && "gap-8",
+              )}
+            >
+              {menu.items.map((item: FooterMenuItem) => (
+                <Typography
+                  font="Lora"
+                  weight="regular"
+                  className="text-lg text-white"
+                  key={typeof item === "string" ? item : item.name}
+                >
+                  {typeof item === "string" ? (
+                    item
+                  ) : (
+                    <>
+                      {menu.title === "Follow Us" ? (
+                        <div className="flex flex-row gap-2 items-center">
+                          <NextImage
+                            src={typeof item !== "string" ? item.icon : ""}
+                            alt={item.name}
+                            width={20}
+                            height={20}
+                            className="inline-block mr-2"
+                          />
+                          <Typography
+                            font="Lora"
+                            weight="regular"
+                            className="text-lg text-white"
+                          >
+                            {item.name}
+                          </Typography>
+                        </div>
+                      ) : (
+                        <NextImage
+                          src={item.icon}
+                          alt={item.name}
+                          width={100}
+                          height={100}
+                          imgClassName="object-cover"
+                        />
+                      )}
+                    </>
+                  )}
+                </Typography>
+              ))}
+            </div>
+          </div>
+        ))}
+      </div>
+    </>
+  );
+};
+
+const FooterDesktopMenu = () => {
+  return (
+    <>
+      <div className="desktop flex-col w-full gap-28 hidden sm:flex">
+        <div className="atas flex w-full flex-row gap-12 justify-between">
+          {FooterMenu.slice(0, 3).map((menu: FooterMenuProps, index) => (
+            <div key={index} className="each flex w-full gap-9 flex-col">
               <Typography
                 font="Lora"
                 weight="bold"
-                className="text-xl text-white"
+                className="text-xl text-white xl:text-3xl lg:text-2xl md:text-xl"
               >
                 {menu.title}
               </Typography>
-              <div
-                className={clsxm(
-                  "isi w-full flex flex-col gap-3",
-                  menu.title === "Payment" && "gap-8",
-                )}
-              >
+              <div className={clsxm("isi w-full flex flex-col gap-5")}>
                 {menu.items.map((item: FooterMenuItem) => (
                   <Typography
                     font="Lora"
                     weight="regular"
-                    className="text-lg text-white"
+                    className="text-xl text-white xl:text-3xl lg:text-2xl md:text-xl"
                     key={typeof item === "string" ? item : item.name}
                   >
                     {typeof item === "string" ? (
@@ -110,7 +173,7 @@ export default function Footer() {
                             <Typography
                               font="Lora"
                               weight="regular"
-                              className="text-lg text-white"
+                              className="text-lg text-white xl:text-3xl lg:text-2xl md:text-xl"
                             >
                               {item.name}
                             </Typography>
@@ -132,21 +195,98 @@ export default function Footer() {
             </div>
           ))}
         </div>
+        <div className="bawah w-full flex flex-row gap-4 justify-between items-start">
+          {FooterMenu.slice(3).map((menu: FooterMenuProps, index) => (
+            <div
+              key={index}
+              className={clsxm(
+                "each flex gap-8 flex-col",
+                menu.title === "Payment" && "w-[65%]",
+              )}
+            >
+              <Typography
+                font="Lora"
+                weight="bold"
+                className="text-xl text-white xl:text-3xl lg:text-2xl md:text-xl"
+              >
+                {menu.title}
+              </Typography>
+              <div
+                className={clsxm(
+                  "isi w-full flex",
+                  menu.title === "Payment" && "flex-wrap gap-9",
+                  menu.title === "Follow Us" && "flex-col gap-6",
+                )}
+              >
+                {menu.items
+                  .filter((item: FooterMenuItem) => typeof item !== "string")
+                  .map((item: FooterMenuItem) => (
+                    <div
+                      key={typeof item === "string" ? item : item.name}
+                      className="text-lg text-white w-fit"
+                    >
+                      {menu.title === "Follow Us" &&
+                      typeof item !== "string" ? (
+                        <div className="flex flex-row gap-2 items-center">
+                          <NextImage
+                            src={item.icon}
+                            alt={item.name}
+                            width={20}
+                            height={20}
+                            className="inline-block mr-2 lg:w-9 sm:w-7"
+                            imgClassName="w-full object-cover"
+                          />
+                          <Typography
+                            font="Lora"
+                            weight="regular"
+                            className="text-xl text-white xl:text-3xl lg:text-2xl md:text-xl"
+                          >
+                            {item.name}
+                          </Typography>
+                        </div>
+                      ) : typeof item !== "string" ? (
+                        <NextImage
+                          src={item.icon}
+                          alt={item.name}
+                          width={120}
+                          height={120}
+                          imgClassName="object-cover"
+                        />
+                      ) : null}
+                    </div>
+                  ))}
+              </div>
+            </div>
+          ))}
+        </div>
       </div>
-      <div className="w-full flex flex-col gap-9 justify-center items-center">
+    </>
+  );
+};
+
+export default function Footer() {
+  return (
+    <footer className="w-full relative flex flex-col gap-16 px-8 py-10 bg-[#525B44] xl:px-24 lg:px-20 md:px-16 sm:px-8 xl:py-20 lg:py-16 md:py-12">
+      <div className="w-full flex flex-col gap-10 sm:gap-32">
+        <FooterDesktopMenu />
+        <FooterMobileMenu />
+      </div>
+      <div className="w-full flex flex-col gap-9 justify-center items-center ">
         <div className="divider w-full h-0 border border-white"></div>
-        <div className="w-full flex flex-col gap-5 justify-center items-center">
-          <div className="logoNtitle flex flex-row gap-3 justify-center items-center w-fit">
+        <div className="w-full flex flex-col gap-5 justify-center items-center sm:flex-row sm:gap-4 sm:justify-between">
+          <div className="logoNtitle flex flex-row gap-3 justify-center items-center w-fit md:gap-5">
             <NextImage
               src="/navbar/Logo.svg"
               alt="logo"
               width={50}
               height={42}
+              className="xl:w-32 lg:w-28 md:w-24 sm:w-20"
+              imgClassName="w-full"
             />
             <Typography
               font="Lora"
               weight="bold"
-              className="text-white text-base relative top-1"
+              className="text-white text-base relative top-1 xl:text-3xl lg:text-2xl md:text-lg sm:text-base sm:top-2"
             >
               DESIGN SIMPLIFIED
             </Typography>
@@ -154,7 +294,7 @@ export default function Footer() {
           <Typography
             font="Lora"
             weight="regular"
-            className="text-white text-xs text-center"
+            className="text-white text-xs text-center xl:text-3xl lg:text-2xl md:text-lg sm:text-sm"
           >
             2024 Design Simplified. All Rights Reserved
           </Typography>
