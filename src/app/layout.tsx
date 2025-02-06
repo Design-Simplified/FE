d9229fb7b214b340743301bf16fd8cce6a0ddbb0
qiqioberon <aquq1q1.farrukh@gmail.com>
2025-02-06 21:58:34 +0700

    feat: integrate chatbot

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -26,6 +26,22 @@ export default function RootLayout({
       <head>
         <link rel="icon" href="/DesignSimplified.ico" />
       </head>
+      <script
+        type="text/javascript"
+        dangerouslySetInnerHTML={{
+          __html: `
+      var Tawk_API=Tawk_API||{}, Tawk_LoadStart=new Date();
+      (function(){
+        var s1=document.createElement("script"),s0=document.getElementsByTagName("script")[0];
+        s1.async=true;
+        s1.src='https://embed.tawk.to/679e32f73a84273260788803/1ij0ve74m';
+        s1.charset='UTF-8';
+        s1.setAttribute('crossorigin','*');
+        s0.parentNode.insertBefore(s1,s0);
+      })();
+    `,
+        }}
+      />
       <body className={clsxm("w-full", lora.className)}>
         <Providers>{children}</Providers>
       </body>
