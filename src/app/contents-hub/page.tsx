51b8985a36a185325b16a45b0b171741cbee7e4c
qiqioberon <aquq1q1.farrukh@gmail.com>
2025-02-08 03:24:49 +0700

    feat: add second section content hub mobile n desktop

--- src/app/contents-hub/page.tsx
+++ src/app/contents-hub/page.tsx
@@ -1,11 +1,13 @@
 import Layout from "@/layouts/Layout";
 import FirstSection from "./container/FirstSection";
+import SecondSection from "./container/SecondSection";
 
 export default function ContentHubPage() {
   return (
     <Layout withFooter={true} withNavbar={true}>
       <main className="w-full contents-hub">
         <FirstSection />
+        <SecondSection />
       </main>
     </Layout>
   );
